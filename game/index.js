d77df12580adf9b4f19c80a02900cc265fc6566a
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2024-03-28 16:34:33 +0000

    Update index.js

--- game/index.js
+++ game/index.js
@@ -25,8 +25,8 @@ for (let i=0; i<battleZonesData.length; i+=70){
 
 const boundaries = [];
 const offset = {
-    x: -250,
-    y: -500,
+    x: 250,
+    y: 500,
 }
 
 collisionsMap.forEach((row, i) => {
