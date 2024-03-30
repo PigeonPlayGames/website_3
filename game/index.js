f225ff8be454dcede2631021d4cd2e391887f0d0
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2024-03-30 23:11:31 +0000

    Update index.js

--- game/index.js
+++ game/index.js
@@ -46,7 +46,7 @@ const bgImage = new Image();
 bgImage.src = "./img/Galley_hill3.png";
 
 const foregroundImage = new Image();
-foregroundImage.src = "./img/Foreground_galley_moved12.png";
+foregroundImage.src = "./img/Foreground_galley_moved13.png";
 
 const playerUpImage = new Image();
 playerUpImage.src = "./img/playerUp.png";
